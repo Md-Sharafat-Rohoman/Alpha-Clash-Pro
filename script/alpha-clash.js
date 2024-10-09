c898f021c613589e17e2da8ec6a891495bb0d312
Google Account <mdsharafatrohomanzafourllah@example.com>
2024-10-10 02:05:50 +0600

    'full'

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,3 +1,31 @@
-function play(){
-    console.log('play start in now')
-}
+/* function play(){
+    const homeSection = document.getElementById('home-screen');
+    homeSection.classList.add('hidden');
+
+    const playGroundSection = document.getElementById('play-ground');
+    playGroundSection.classList.remove('hidden')
+} */
+
+
+    function continueGame(){
+        const alphabet = getARandomAlphabet();
+        // console.log('your random alphabet :', alphabet)
+
+        const currentAlphabetString = document.getElementById('current-alphabet');
+        currentAlphabetString.innerText = alphabet;
+
+        setBackgrounColor(alphabet);
+
+
+
+    }
+
+
+
+
+    function play(){
+        hideElementById('home-screen');
+        showElementById('play-ground');
+        continueGame();
+
+    }
